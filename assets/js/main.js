a916af150bbadef171dc60749fff7139bc423b1b
Alberto <dany.canaviri2022@gmail.com>
2025-11-23 23:54:42 -0400

    Card Design JavaScript and Breakpoints, completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 /*=============== SHOW MENU ===============*/
+const showMenu = (btnId, menuId) => {
+    const btn = document.getElementById(btnId), 
+          menu = document.getElementById(menuId)
+
+    btn.addEventListener('click', () => {
+        menu.classList.toggle('show-menu')
+        btn.classList.toggle('show-icon')
+    })
+}
+
+showMenu('card-btn', 'card-menu')
